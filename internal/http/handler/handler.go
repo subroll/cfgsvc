9f52f3502155d23c890250ff5a0fabc845e64b70
wisnu <wisnu.septiaji@gmail.com>
2019-11-03 22:58:42 +0700

    completing item.get http handler

--- internal/http/handler/handler.go
+++ internal/http/handler/handler.go
@@ -1,2 +1,89 @@
 // Package handler provides the http handler.
 package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/subroll/cfgsvc/internal/config"
+
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	badRequestResponse = response{
+		HTTPCode: http.StatusBadRequest,
+		Message:  "Bad Request",
+	}
+	notFoundResponse = response{
+		HTTPCode: http.StatusNotFound,
+		Message:  "Not Found",
+	}
+	internalServerErrorResponse = response{
+		HTTPCode: http.StatusInternalServerError,
+		Message:  "Internal Server Error",
+	}
+)
+
+type response struct {
+	HTTPCode int         `json:"-"`
+	Message  string      `json:"message,omitempty"`
+	Data     interface{} `json:"data,omitempty"`
+}
+
+type itemsResponse struct {
+	GroupID   int       `json:"group_id"`
+	GroupName string    `json:"group_name"`
+	UpdatedAt time.Time `json:"updated_at"`
+	CreatedAt time.Time `json:"created_at"`
+	Items     []struct {
+		ID        int       `json:"id"`
+		Key       string    `json:"key"`
+		Value     string    `json:"value"`
+		UpdatedAt time.Time `json:"updated_at"`
+		CreatedAt time.Time `json:"created_at"`
+	} `json:"items"`
+}
+
+func writeResponse(rw http.ResponseWriter, r response) {
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(r.HTTPCode)
+
+	if err := json.NewEncoder(rw).Encode(r); err != nil {
+		log.WithError(err).Error("fail to encode response to response writer")
+	}
+}
+
+func makeGroupedItemsResponse(items []config.GroupedItems) response {
+	confSlice := make([]itemsResponse, len(items))
+	resp := response{
+		HTTPCode: http.StatusOK,
+		Data:     confSlice,
+	}
+	for i := 0; i < len(items); i++ {
+		confSlice[i] = itemsResponse{
+			GroupID:   items[i].GroupID,
+			GroupName: items[i].GroupName,
+			UpdatedAt: items[i].UpdatedAt,
+			CreatedAt: items[i].CreatedAt,
+		}
+		for _, item := range items[i].Items {
+			confSlice[i].Items = append(confSlice[i].Items, struct {
+				ID        int       `json:"id"`
+				Key       string    `json:"key"`
+				Value     string    `json:"value"`
+				UpdatedAt time.Time `json:"updated_at"`
+				CreatedAt time.Time `json:"created_at"`
+			}{
+				ID:        item.ID,
+				Key:       item.Key,
+				Value:     item.Value,
+				UpdatedAt: item.UpdatedAt,
+				CreatedAt: item.CreatedAt,
+			})
+		}
+	}
+
+	return resp
+}
--- internal/http/handler/item.go
+++ internal/http/handler/item.go
@@ -2,6 +2,9 @@ package handler
 
 import (
 	"net/http"
+	"strconv"
+
+	log "github.com/sirupsen/logrus"
 
 	"github.com/subroll/cfgsvc/internal/config"
 )
@@ -18,6 +21,56 @@ type Item struct {
 
 // Get is the http handler for /config
 func (i *Item) Get(rw http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	rawItemID := r.URL.Query().Get("id")
+	rawGroupID := r.URL.Query().Get("group_id")
+	var err error
+
+	var itemID int
+	if len(rawItemID) > 0 {
+		itemID, err = strconv.Atoi(rawItemID)
+		if err != nil {
+			log.WithError(err).Error("can not convert id to int")
+			writeResponse(rw, badRequestResponse)
+			return
+		}
+		if itemID == 0 {
+			log.Warn("id is zero")
+			writeResponse(rw, notFoundResponse)
+			return
+		}
+	}
+
+	var groupID int
+	if len(rawGroupID) > 0 {
+		groupID, err = strconv.Atoi(rawGroupID)
+		if err != nil {
+			log.WithError(err).Error("can not convert group_id to int")
+			writeResponse(rw, badRequestResponse)
+			return
+		}
+		if groupID == 0 {
+			log.Warn("group_id is zero")
+			writeResponse(rw, notFoundResponse)
+			return
+		}
+	}
+
+	items, err := i.itemSvc.GroupedItems(ctx, groupID, itemID)
+	if err != nil {
+		if err == config.ErrNotFound {
+			log.WithError(err).Warn("no config item matched the query")
+			writeResponse(rw, notFoundResponse)
+			return
+		}
+
+		log.WithError(err).Error("fail to get item list")
+		writeResponse(rw, internalServerErrorResponse)
+		return
+	}
+
+	writeResponse(rw, makeGroupedItemsResponse(items))
+	return
 }
 
 // Post is the http handler for /config
